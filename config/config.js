93d29176a7fd7472963d0c192b6d796061e00461
Camille Stacho <camillestacho@camilles-mbp.lan>
2019-01-14 15:27:56 -0500

    unignoring config

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb://freegan:freegan11@ds257314.mlab.com:57314/freegan',
+    db_dev: 'mongodb://freegan:freegan11@ds257314.mlab.com:57314/freegan',
+    collection_name: ''
+   };
