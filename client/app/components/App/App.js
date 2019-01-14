144095dd80898bec35fc9c3b5377dd1a01f97dce
mailaw <mlaw534@gmail.com>
2019-01-14 14:42:57 -0500

    header styling

--- client/app/components/App/App.js
+++ client/app/components/App/App.js
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
+const selectStyle = {
+  minWidth: "150px"
+};
+
 const App = ({ children }) => (
   <>
     <Header />
--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -1,18 +1,31 @@
 import React from "react";
+//import styles from "../../styles/styles.scss";
 
 import { Link } from "react-router-dom";
+const headerStyle = {
+  backgroundColor: "ivory",
+  fontSize: "20px",
+  color: "pink"
+};
+const navStyle = {
+  display: "inline-block",
+  width: "90px"
+};
 
 const Header = () => (
-  <header>
-    <Link to="/">Home</Link>
-
-    <nav>
-      <Link to="/helloworld">Hello World</Link>
-    </nav>
-    <nav>
-      <Link to="/directory">Directory</Link>
-    </nav>
-
+  <header style={headerStyle}>
+    <h1>Freegan Directory</h1>
+    <div>
+      <nav style={navStyle}>
+        <Link to="/">Home</Link>
+      </nav>
+      <nav style={navStyle}>
+        <Link to="/helloworld">Hello World</Link>
+      </nav>
+      <nav style={navStyle}>
+        <Link to="/directory">Directory</Link>
+      </nav>
+    </div>
     <hr />
   </header>
 );
