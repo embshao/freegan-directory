ea9a18a88e4434fd2c425bfc39ad3e5998898771
mailaw <mlaw534@gmail.com>
2019-01-14 20:43:44 -0500

    functional test feed

--- client/app/components/Feed/Feed.js
+++ client/app/components/Feed/Feed.js
@@ -7,11 +7,27 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
 
+var siteList = [];
+
+//just testing
+const testList = [
+  { description: "Starbucks", id: "2", created_at: "2014-03-08 01:35:48 UTC" },
+  { description: "Juice Town", id: "4", created_at: "2014-03-08 01:35:48 UTC" },
+  { description: "Best Bagel", id: "5", created_at: "2014-03-08 01:35:48 UTC" }
+];
+//const listTest = testList.map(test => <li>{test.description}</li>);
+
 class Feed extends Component {
   constructor(props) {
     super(props);
   }
+
   componentDidMount() {
+    //time now
+    var start = new Date();
+    var currentTime = start.toUTCString();
+    console.log(currentTime);
+
     let url = "/freegan";
     let http = new XMLHttpRequest();
     http.open("GET", url, true);
@@ -21,9 +37,16 @@ class Feed extends Component {
         if (http.status === 200) {
           console.log("wassup connected");
           let res = JSON.parse(http.response);
-          console.log(res);
           for (let row of res) {
-            console.log(row.created_at);
+            //turn created_at into seconds
+            var siteTime = row.created_at;
+            //console.log(siteTime);
+            if (siteTime > "2014-03-06 01:35:48 UTC") {
+              //3 days
+              //siteList.push(row);
+              siteList.push(row.description);
+              //console.log(row.description);
+            }
           }
         }
       }
@@ -35,28 +58,18 @@ class Feed extends Component {
       <div className="feed-body">
         <h1> Visit Feed</h1>
         <Dropdown />
-
         <List>
-          <ListItem button>
-            <ListItemText primary="Dumpster outside of Associated" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            {/*<ListItemIcon>*/}
-            <ListItemText primary="Starbucks" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText primary="Best Bagel" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText primary="Juice Town" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText primary="Sunrise Deli" />
-          </ListItem>
+          {testList.map(test => (
+            <div>
+              <ListItem button>
+                <ListItemText
+                  primary={test.description}
+                  secondary={test.created_at}
+                />
+              </ListItem>
+              <Divider />
+            </div>
+          ))}
         </List>
       </div>
     );
